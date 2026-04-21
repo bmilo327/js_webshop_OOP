eff895d5b0a958553470aae23ec3bd1fdf996838
BumbleVeee <gubek.veronika.eva@gmail.com>
2026-04-21 13:11:33 +0200

    megjelenites kijavitva, még kell a lista megjelenitese

--- KartyaAdmin.js
+++ KartyaAdmin.js
@@ -1,27 +1,28 @@
 export default class KartyaAdmin {
     #obj = {}
     constructor(obj = {index, nev, leiras, kep, ar, ertekeles}, szuloElem){
-        this.#obj = obj,
-        this.szuloElem = szuloElem,
+        this.#obj = obj;
+        this.szuloElem = szuloElem;
         this.megjelenit()
-    }
-
-    getObj(){
-        return this.#obj
     }
 
     megjelenit(){
         let kod =`
             <tr>
-                <th>${this.#obj.nev}</th>
-                <th>${this.#obj.leiras}</th>
-                <th><img src="${this.#obj.kep}" alt="${this.#obj.index}"></th>
-                <th>${this.#obj.ar}</th>
-                <th>${this.#obj.ertekeles}</th>
-                <th><button class="hozaadas">✓</button></th>
-                <th><button class="torles">✕</button></th>
+                <td>${this.#obj.nev}</td>
+                <td>${this.#obj.leiras}</td>
+                <td><img src="${this.#obj.kep}" alt="${this.#obj.index}"></td>
+                <td>${this.#obj.ar}</td>
+                <td>${this.#obj.ertekeles}</td>
+                <td><td>
+                <td><button class="hozaadas">✓</button></td>
+                <td><button class="torles">✕</button></td>
             </tr>
         `
-        this.szuloElem.insertAdjacentHTML("beforeend", kod)
+        this.szuloElem.insertAdjacentHTML("beforeend", kod);
+    }
+
+    getObj(){
+        return this.#obj
     }
 }
